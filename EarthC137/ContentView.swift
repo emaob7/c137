6addfe55304397ad18cbfc2b397020744a459611
emaob7 <emanuelob7@gmail.com>
2024-08-14 19:28:38 -0400

    Initial Commit

---
+++ EarthC137/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EarthC137
+//
+//  Created by Emmanuel Ortigoza Bordón on 2024-08-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EarthC137/EarthC137App.swift
@@ -0,0 +1,17 @@
+//
+//  EarthC137App.swift
+//  EarthC137
+//
+//  Created by Emmanuel Ortigoza Bordón on 2024-08-14.
+//
+
+import SwiftUI
+
+@main
+struct EarthC137App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
